f345972ff1532f0ea66ffd95de36bc2780fb7d8c
Sami Ullah <muftisami2010@gmail.com>
2025-09-29 16:51:52 +0530

    fix

--- android/src/main/java/nl/volst/HoneywellScanner/HoneywellScannerModule.java
+++ android/src/main/java/nl/volst/HoneywellScanner/HoneywellScannerModule.java
@@ -99,7 +99,7 @@ public class HoneywellScannerModule extends ReactContextBaseJavaModule implement
                         reader.setProperty(BarcodeReader.PROPERTY_CODE_128_ENABLED, true);
                         reader.setProperty(BarcodeReader.PROPERTY_CODE_39_ENABLED, true);
                         promise.resolve(true);
-                    } catch (ScannerUnavailableException | UnsupportedPropertyException | Exception e) {
+                    } catch (ScannerUnavailableException | UnsupportedPropertyException e) {
                         promise.resolve(false);
                         e.printStackTrace();
                     }

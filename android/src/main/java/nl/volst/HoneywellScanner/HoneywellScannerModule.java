bdf1916512982acb1a1ea733740ded8c95ab19f4
Dilin Alwis <dalwis@noon.com>
2026-02-06 14:41:55 +0400

    Fixed EAN 13 code 93 releated symbology for barcode reading

--- android/src/main/java/nl/volst/HoneywellScanner/HoneywellScannerModule.java
+++ android/src/main/java/nl/volst/HoneywellScanner/HoneywellScannerModule.java
@@ -100,6 +100,13 @@ public class HoneywellScannerModule extends ReactContextBaseJavaModule implement
                         reader.setProperty(BarcodeReader.PROPERTY_CODE_128_ENABLED, true);
                         reader.setProperty(BarcodeReader.PROPERTY_CODE_39_ENABLED, true);
                         reader.setProperty(BarcodeReader.PROPERTY_CODE_39_FULL_ASCII_ENABLED, true);
+                        reader.setProperty(BarcodeReader.PROPERTY_EAN_8_CHECK_DIGIT_TRANSMIT_ENABLED, true);
+                        reader.setProperty(BarcodeReader.PROPERTY_EAN_13_CHECK_DIGIT_TRANSMIT_ENABLED, true);
+                        reader.setProperty(BarcodeReader.PROPERTY_UPC_A_CHECK_DIGIT_TRANSMIT_ENABLED, true);
+                        reader.setProperty(BarcodeReader.PROPERTY_UPC_E_CHECK_DIGIT_TRANSMIT_ENABLED, true);
+                        reader.setProperty(BarcodeReader.PROPERTY_POSTAL_2D_PLANET_CHECK_DIGIT_TRANSMIT_ENABLED, true);
+                        reader.setProperty(BarcodeReader.PROPERTY_POSTAL_2D_POSTNET_CHECK_DIGIT_TRANSMIT_ENABLED, true);
+                        reader.setProperty(BarcodeReader.PROPERTY_CODE_93_ENABLED, true);
                         promise.resolve(true);
                     } catch (ScannerUnavailableException | UnsupportedPropertyException e) {
                         promise.resolve(false);
